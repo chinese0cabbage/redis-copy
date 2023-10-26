b23823ccc7a929fbacbdca1f784bafe39442a307
zhangjun <1922585388@qq.com>
2023-10-26 22:02:27 +0800

    debug sds

--- sds.h
+++ sds.h
@@ -51,14 +51,14 @@ struct PACKED sdshdr64{
 #define SDS_TYPE_64 4
 #define SDS_TYPE_MASK 7
 #define SDS_TYPE_BITS 3
-#define SDS_HDR_VAR(T, s) struct sdshdr##t *sh = (void *)((s) - (sizeof(struct sdshdr##T)))
+#define SDS_HDR_VAR(T, s) struct sdshdr##T *sh = (void *)((s) - (sizeof(struct sdshdr##T)))
 #define SDS_HDR(T, S) ((struct sdshdr##T *)((s) - (sizeof(struct sdshdr##T))))
 #define SDS_TYPE_5_LEN(f) ((f)>>SDS_TYPE_BITS)
 
 static inline size_t sdslen(const sds s){
     uint8_t flags = s[-1];
 
-    switch(flags && SDS_TYPE_MASK){
+    switch(flags & SDS_TYPE_MASK){
         case SDS_TYPE_5:
             return SDS_TYPE_5_LEN(flags);
         case SDS_TYPE_8:
@@ -72,3 +72,125 @@ static inline size_t sdslen(const sds s){
     }
     return 0;
 }
+
+static inline size_t sdsavil(const sds s){
+    uint8_t flags = s[-1];
+    switch(flags & SDS_TYPE_MASK){
+        case SDS_TYPE_5:{
+            return 0;
+        }
+        case SDS_TYPE_8:{
+            SDS_HDR_VAR(8, s);
+            return sh->alloc - sh->len;
+        }
+        case SDS_TYPE_16:{
+            SDS_HDR_VAR(16, s);
+            return sh->alloc - sh->len;
+        }
+        case SDS_TYPE_32:{
+            SDS_HDR_VAR(32, s);
+            return sh->alloc - sh->len;
+        }
+        case SDS_TYPE_64:{
+            SDS_HDR_VAR(64, s);
+            return sh->alloc - sh->len;
+        }
+    }
+    return 0;
+}
+
+static inline void sdssetlen(sds s, size_t newlen){
+    uint8_t flags = s[-1];
+    switch(flags & SDS_TYPE_MASK){
+        case SDS_TYPE_5:{
+            uint8_t *fp = ((uint8_t *)s) - 1;
+            *fp = SDS_TYPE_5 | (newlen << SDS_TYPE_BITS);
+        }
+        case SDS_TYPE_8:
+            SDS_HDR(8, s)->len = newlen;
+            break;
+        case SDS_TYPE_16:
+            SDS_HDR(16, s)->len = newlen;
+            break;
+        case SDS_TYPE_32:
+            SDS_HDR(32, s)->len = newlen;
+            break;
+        case SDS_TYPE_64:
+            SDS_HDR(64, s)->len = newlen;
+            break;
+    }
+}
+
+static inline void sdsinclen(sds s, size_t inc){
+    uint8_t flags = s[-1];
+    switch(flags & SDS_TYPE_MASK){
+        case SDS_TYPE_5:{
+            uint8_t *fp = ((uint8_t *)s) - 1;
+            uint8_t newlen = SDS_TYPE_5_LEN(flags) + inc;
+            *fp = SDS_TYPE_5 | (newlen << SDS_TYPE_BITS);
+        }
+            break;
+        case SDS_TYPE_8:
+            SDS_HDR(8, s)->len += inc;
+            break;
+        case SDS_TYPE_16:
+            SDS_HDR(16, s)->len +=inc;
+            break;
+        case SDS_TYPE_32:
+            SDS_HDR(32, s)->len += inc;
+            break;
+        case SDS_TYPE_64:
+            SDS_HDR(64, s)->len += inc;
+            break;
+    }
+}
+
+static inline size_t sdsalloc(const sds s){
+    uint8_t flags = s[-1];
+    switch(flags & SDS_TYPE_MASK){
+        case SDS_TYPE_5:
+            return SDS_TYPE_5_LEN(flags);
+        case SDS_TYPE_8:
+            return SDS_HDR(8, s)->alloc;
+        case SDS_TYPE_16:
+            return SDS_HDR(16, s)->alloc;
+        case SDS_TYPE_32:
+            return SDS_HDR(32, s)->alloc;
+        case SDS_TYPE_64:
+            return SDS_HDR(64, s)->alloc;
+    }
+    return 0;
+}
+
+static inline void sdssetalloc(sds s, size_t newlen){
+    uint8_t flags = s[-1];
+    switch(flags & SDS_TYPE_MASK){
+        case SDS_TYPE_5:
+            break;
+        case SDS_TYPE_8:
+            SDS_HDR(8, s)->alloc = newlen;
+            break;
+        case SDS_TYPE_16:
+            SDS_HDR(16, s)->alloc = newlen;
+            break;
+        case SDS_TYPE_32:
+            SDS_HDR(32, s)->alloc = newlen;
+            break;
+        case SDS_TYPE_64:
+            SDS_HDR(64, s)->alloc = newlen;
+            break;
+    }
+}
+
+sds sdsnewlen(const void *init, size_t initlen);
+sds sdstrynewlen(const void *init, size_t initlen);
+sds sdsnew(const char *init);
+sds sdsempty(void);
+sds sdsdup(const sds s);
+void sdsfree(sds s);
+sds sdsgrowzero(sds s, size_t len);
+sds sdscatlen(sds s, const void *t, size_t len);
+sds sdscat(sds s, const char *t);
+sds sdscatsds(sds s, const sds t);
+sds sdscpylen(sds s, const char *t, size_t len);
+sds sdscpy(sds s, const char *t);
